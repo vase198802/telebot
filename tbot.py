41e935887a58c776d3e0f140a863685f5c299c74
vasya <vase198802@gmaul.com>
2024-09-29 17:56:36 +0300

    Initial commit1

--- tbot.py
+++ tbot.py
@@ -1,65 +1,152 @@
-# Подключаем модуль случайных чисел
-import random
-# Подключаем модуль для Телеграма
-import telebot
-# Указываем токен
-bot = telebot.TeleBot('7865327651:AAGVWWZKhOutjFnBc_XR-19s6p1LCAPOjfs')
-# Импортируем типы из модуля, чтобы создавать кнопки
+import requests
 from telebot import types
-# Заготовки для трёх предложений
-first = ["Сегодня — идеальный день для новых начинаний.","Оптимальный день для того, чтобы решиться на смелый поступок!","Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.","Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.","Плодотворный день для того, чтобы разобраться с накопившимися делами."]
-second = ["Но помните, что даже в этом случае нужно не забывать про","Если поедете за город, заранее подумайте про","Те, кто сегодня нацелен выполнить множество дел, должны помнить про","Если у вас упадок сил, обратите внимание на","Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"]
-second_add = ["отношения с друзьями и близкими.","работу и деловые вопросы, которые могут так некстати помешать планам.","себя и своё здоровье, иначе к вечеру возможен полный раздрай.","бытовые вопросы — особенно те, которые вы не доделали вчера.","отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."]
-third = ["Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.","Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.","Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.","Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.","Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."]
-# Метод, который получает сообщения и обрабатывает их
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    # Если написали «Привет»
-    if message.text == "Привет":
-        # Пишем приветствие
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
-        # Готовим кнопки
-        keyboard = types.InlineKeyboardMarkup()
-        # По очереди готовим текст и обработчик для каждого знака зодиака
-        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
-        # И добавляем кнопку на экран
-        keyboard.add(key_oven)
-        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
-        keyboard.add(key_telec)
-        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
-        keyboard.add(key_bliznecy)
-        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
-        keyboard.add(key_rak)
-        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
-        keyboard.add(key_lev)
-        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
-        keyboard.add(key_deva)
-        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
-        keyboard.add(key_vesy)
-        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
-        keyboard.add(key_scorpion)
-        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
-        keyboard.add(key_strelec)
-        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
-        keyboard.add(key_kozerog)
-        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
-        keyboard.add(key_vodoley)
-        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
-        keyboard.add(key_ryby)
-        # Показываем все кнопки сразу и пишем сообщение о выборе
-        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши Привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-# Обработчик нажатий на кнопки
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    # Если нажали на одну из 12 кнопок — выводим гороскоп
-    if call.data == "zodiac":
-        # Формируем гороскоп
-        msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
-        # Отправляем текст в Телеграм
-        bot.send_message(call.message.chat.id, msg)
-# Запускаем постоянный опрос бота в Телеграме
-bot.polling(none_stop=True, interval=0)
+import telebot
+from bs4 import BeautifulSoup as b
+
+URL1 = 'https://horo.mail.ru/prediction/aries/today/'
+API_KEY = '5737874370:AAEACwtnBOUwa2UzkEX4Q6Gnnz8cDddZ6ic'
+def parser_aries(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_aries = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_aries]
+
+list_aries = parser_aries(URL1)
+#Телец
+URL2 = 'https://horo.mail.ru/prediction/taurus/today/'
+def parser_taurus(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_taurus = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_taurus]
+list_taurus = parser_taurus(URL2)
+#Близнецы
+URL3 = 'https://horo.mail.ru/prediction/gemini/today/'
+def parser_twins(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_twins = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_twins]
+list_twins = parser_twins(URL3)
+#Рак
+URL4 = 'https://horo.mail.ru/prediction/cancer/today/'
+def parser_crayfish(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_crayfish = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_crayfish]
+list_crayfish = parser_crayfish(URL4)
+#Лев
+URL5 = 'https://horo.mail.ru/prediction/leo/today/'
+def parser_lion(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_lion = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_lion]
+list_lion = parser_lion(URL5)
+#Дева
+URL6 = 'https://horo.mail.ru/prediction/virgo/today/'
+def parser_virgo(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_virgo = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_virgo]
+list_virgo = parser_virgo(URL6)
+#Весы
+URL7 = 'https://horo.mail.ru/prediction/libra/today/'
+def parser_scales(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_scales = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_scales]
+list_scales = parser_scales(URL7)
+#Скорпион
+URL8 = 'https://horo.mail.ru/prediction/scorpio/today/'
+def parser_scorpion(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_scorpion = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_scorpion]
+list_scorpion = parser_scorpion(URL8)
+#Стрелец
+URL9 = 'https://horo.mail.ru/prediction/sagittarius/today/'
+def parser_sagittarius(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_sagittarius = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_sagittarius]
+list_sagittarius = parser_sagittarius(URL9)
+#Козерог
+URL10 = 'https://horo.mail.ru/prediction/capricorn/today/'
+def parser_capricorn(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_capricorn = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_capricorn]
+list_capricorn = parser_capricorn(URL10)
+#Водолей
+URL11 = 'https://horo.mail.ru/prediction/aquarius/today/'
+def parser_aquarius(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_aquarius = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_aquarius]
+list_aquarius = parser_aquarius(URL11)
+#Рыбы
+URL12 = 'https://horo.mail.ru/prediction/pisces/today/'
+def parser_fish(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    horoscope_fish = soup.find_all('div', class_='article__text')
+    return [c.text for c in horoscope_fish]
+list_fish = parser_fish(URL12)
+
+bot = telebot.TeleBot('8039836946:AAHyyI4156WFz1Oe97H5chQnAB6it43GEso')
+@bot.message_handler(['начать'])
+def hello(message):
+    button = types.InlineKeyboardMarkup(row_width=4)
+    cnopka = types.InlineKeyboardButton('Овен', callback_data='1')
+    cnopka2 = types.InlineKeyboardButton('Телец', callback_data='2')
+    cnopka3 = types.InlineKeyboardButton('Близнецы', callback_data='3')
+    cnopka4 = types.InlineKeyboardButton('Рак', callback_data='4')
+    cnopka5 = types.InlineKeyboardButton('Лев', callback_data='5')
+    cnopka6 = types.InlineKeyboardButton('Дева', callback_data='6')
+    cnopka7 = types.InlineKeyboardButton('Весы', callback_data='7')
+    cnopka8 = types.InlineKeyboardButton('Скорпион', callback_data='8')
+    cnopka9 = types.InlineKeyboardButton('Стрелец', callback_data='9')
+    cnopka10 = types.InlineKeyboardButton('Козерог', callback_data='10')
+    cnopka11 = types.InlineKeyboardButton('Водолей', callback_data='11')
+    cnopka12 = types.InlineKeyboardButton('Рыбы', callback_data='12')
+    button.add(cnopka, cnopka2, cnopka3, cnopka4, cnopka5, cnopka6, cnopka7, cnopka8, cnopka9, cnopka10, cnopka11, cnopka12)
+    bot.send_message(message.chat.id, 'Привет! Гороскоп на сегодня выбери свой знак задиака:', reply_markup=button)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback(call):
+    if call.message:
+        if call.data == '1':
+            bot.send_message(call.message.chat.id, list_aries[0])
+        elif call.data == '2':
+            bot.send_message(call.message.chat.id, list_taurus[0])
+        elif call.data == '3':
+            bot.send_message(call.message.chat.id, list_twins[0])
+        elif call.data == '4':
+            bot.send_message(call.message.chat.id, list_crayfish[0])
+        elif call.data == '5':
+            bot.send_message(call.message.chat.id, list_lion[0])
+        elif call.data == '6':
+            bot.send_message(call.message.chat.id, list_virgo[0])
+        elif call.data == '7':
+            bot.send_message(call.message.chat.id, list_scales[0])
+        elif call.data == '8':
+            bot.send_message(call.message.chat.id, list_scorpion[0])
+        elif call.data == '9':
+            bot.send_message(call.message.chat.id, list_sagittarius[0])
+        elif call.data == '10':
+            bot.send_message(call.message.chat.id, list_capricorn[0])
+        elif call.data == '11':
+            bot.send_message(call.message.chat.id, list_aquarius[0])
+        elif call.data == '12':
+            bot.send_message(call.message.chat.id, list_fish[0])
+
+
+bot.polling()
